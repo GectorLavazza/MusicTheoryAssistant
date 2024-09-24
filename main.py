ae9be69a2380a3e637a679135bf73c5bfa1f49bc
pavlen5kiy <150275009+pavlen5kiy@users.noreply.github.com>
2024-09-24 12:01:14 +0300

    Made building menu design

--- main.py
+++ main.py
@@ -7,38 +7,24 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        uic.loadUi("MusicTheoryApp.ui", self)
+
+        uic.loadUi("design.ui", self)
+
+        self.on_init()
+
+    def on_init(self):
         self.setFixedSize(self.width(), self.height())
 
-        self.windows = {
-            'build': self.Build,
-            'train': self.Training,
-        }
+        self.exerciseMenu.setHidden(True)
+        self.settingsMenu.setHidden(True)
 
-        self.current_window = 'build'
-
-        for window in self.windows:
-            if window != 'build':
-                self.windows[window].setVisible(False)
-                self.windows[window].setEnabled(False)
-        self.windows['build'].setVisible(True)
-        self.windows['build'].setEnabled(True)
-
-        self.trainMenuButton.clicked.connect(self.switch_window)
-        self.buildMenuButton.clicked.connect(self.switch_window)
-
-    def switch_window(self):
-        sender = self.sender()
-        switch_to = sender.text().lower()
-        for window in self.windows:
-            if window != switch_to:
-                self.windows[window].setVisible(False)
-                self.windows[window].setEnabled(False)
-        self.windows[switch_to].setVisible(True)
-        self.windows[switch_to].setEnabled(True)
+        self.listWidget.addItem('test 1')
+        self.listWidget.addItem('test 2')
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    app.exec()

6b00eb611222bf3f2e4cbab88eea10b901d61a95
pavlen5kiy <150275009+pavlen5kiy@users.noreply.github.com>
2025-01-09 19:03:30 +0300

    Started working on translations

---
+++ translations_ru.ts
@@ -0,0 +1,364 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>MusicTheoryAssistant</source>
+        <translation type="unfinished">MusicTheoryAssistant</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Music Theory Assistant</source>
+        <translation type="unfinished">Music Theory Assistant</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Build</source>
+        <translation type="unfinished">Построить</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Exercise</source>
+        <translation type="unfinished">Упражнения</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Settings</source>
+        <translation type="unfinished">Настройки</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>v.0.0.1</source>
+        <translation type="unfinished">v.0.0.1</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Scale</source>
+        <translation type="unfinished">Гамма</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Key</source>
+        <translation type="unfinished">Тоника</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>C#\Db</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>D#\Eb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>E</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>F</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>F#\Gb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>G</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>G#\Ab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>A#\Bb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>B\Cb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Major</source>
+        <translation type="unfinished">Мажор</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Minor</source>
+        <translation type="unfinished">Минор</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Major Pent.</source>
+        <translation type="unfinished">Мажор. пентатоника</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Minor Pent.</source>
+        <translation type="unfinished">Минор. пентатоника</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Blues</source>
+        <translation type="unfinished">Блюзовая пентатоника</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Dorian</source>
+        <translation type="unfinished">Дорийский</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Chord</source>
+        <translation type="unfinished">Аккорд</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Augmented</source>
+        <translation type="unfinished">Увеличенный</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Diminished</source>
+        <translation type="unfinished">Уменьшенный</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Add</source>
+        <translation type="unfinished">Доп. ступень</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>None</source>
+        <translation type="unfinished">Нет</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>6</source>
+        <translation type="unfinished">6</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>maj6</source>
+        <translation type="unfinished">maj6</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>7</source>
+        <translation type="unfinished">7</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>maj7</source>
+        <translation type="unfinished">maj7</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Interval</source>
+        <translation type="unfinished">Интервал</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Minor 2nd</source>
+        <translation type="unfinished">Мал. секунда</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Major 2nd</source>
+        <translation type="unfinished">Бол. секунда</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Minor 3rd</source>
+        <translation type="unfinished">Мал. терция</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Major 3rd</source>
+        <translation type="unfinished">Бол.терция</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>4th</source>
+        <translation type="unfinished">Кварта</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Tritone</source>
+        <translation type="unfinished">Тритон</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>5th</source>
+        <translation type="unfinished">Квинта</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Minor 6th</source>
+        <translation type="unfinished">Мал. секста</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Major 6th</source>
+        <translation type="unfinished">Бол. секста</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Minor 7th</source>
+        <translation type="unfinished">Мал. септима</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Major 7th</source>
+        <translation type="unfinished">Бол.септима</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Octave</source>
+        <translation type="unfinished">Октава</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Listen</source>
+        <translation type="unfinished">Прослушать</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Search</source>
+        <translation type="unfinished">Найти</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Language</source>
+        <translation type="unfinished">Язык</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>English</source>
+        <translation type="unfinished">English</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Русский</source>
+        <translation type="unfinished">Русски</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Instrument</source>
+        <translation type="unfinished">Инструмент</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Piano</source>
+        <translation type="unfinished">Фортепиано</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Guitar</source>
+        <translation type="unfinished">Гитара</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Reset</source>
+        <translation type="unfinished">Сбросить</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Apply</source>
+        <translation type="unfinished">Применить</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Average result: 0</source>
+        <translation type="unfinished">Средний результат: 0</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Notes</source>
+        <translation type="unfinished">Ноты</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Submit</source>
+        <translation type="unfinished">Ответить</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Next</source>
+        <translation type="unfinished">Дальше</translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <location filename="design.ui" line="0"/>
+        <source>Correct answer: </source>
+        <translation type="unfinished">Правильный ответ: </translation>
+    </message>
+    <message>
+        <location filename="design.ui" line="0"/>
+        <source>Intervals</source>
+        <translation type="unfinished">Интервалы</translation>
+    </message>
+</context>
+</TS>
